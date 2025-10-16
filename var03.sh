d24b57d52989f9f42f2c676418249e73cb3d881d
sami <samikshameshram97@gmail.com>
2025-10-16 15:53:34 +0000

    sami-123 | update var file

--- var03.sh
+++ var03.sh
@@ -5,3 +5,11 @@ name="sama"
 age=26
 
 echo "My name is $name and age is $age"
+
+name="shubh"
+echo "My name is $name"
+
+#Var to store the output of a command"
+
+HOSTNAME=$(hostname)
+echo "Name of machine is $HOSTNAME"
